020a6b2f25e9c251a7737ffb3cadeb9baa4198e9
Sanmeshkumar Udhayakumar <sanmesh1@gmail.com>
2025-01-26 10:43:19 -0800

    modified to calculate probability of winning completely

--- risk_win_probability_calculator.py
+++ risk_win_probability_calculator.py
@@ -1,12 +1,15 @@
+#doesnt handle
 def is_dice_hand_winning_in_risk(my_dice, opp_dice):
-    did_i_win = False
-    for my_die in my_dice:
-        for opp_die in opp_dice:
-            if my_die > opp_die:
-                did_i_win = True
-                break
-    print(f"i win? {did_i_win}, my dice: {my_dice}, opp_dice: {opp_dice}")
+    did_i_win = True
+    my_dice_sorted = sorted(my_dice, reverse=True)
+    opp_dice_sorted = sorted(opp_dice, reverse=True)
+    for i in range(min(len(my_dice_sorted), len(opp_dice_sorted))):
+        if my_dice_sorted[i] <= opp_dice_sorted[i]:
+            did_i_win = False
+            break
+    # print(f"i win completely? {did_i_win}, my dice: {my_dice}, opp_dice: {opp_dice}")
     return did_i_win
+
 def dfs(my_dice_count, opp_dice_count, my_dice, opp_dice, total_count, winning_count):
     if len(my_dice) == my_dice_count and len(opp_dice) == opp_dice_count:
         total_count[0]+=1
@@ -29,9 +32,12 @@ def calculate_probability_of_winning_dice_roll(my_dice_count, opp_dice_count):
     my_dice = []
     opp_dice = []
     dfs(my_dice_count, opp_dice_count, my_dice, opp_dice, total_count, winning_count)
-    print(f"{winning_count[0]}/{total_count[0]} = {winning_count[0]/total_count[0]*100}%")
+    print(f"my_dice: {my_dice_count}, opp_dice: {opp_dice_count}, {winning_count[0]}/{total_count[0]} = {winning_count[0]/total_count[0]*100}%")
     
     
 calculate_probability_of_winning_dice_roll(3, 1)
+calculate_probability_of_winning_dice_roll(3, 2)
+calculate_probability_of_winning_dice_roll(2, 1)
+calculate_probability_of_winning_dice_roll(1, 1)
 
 
